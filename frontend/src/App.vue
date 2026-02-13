5247c3bc69ef6525ac30154b6d7d7a6d91d1355a
Jakub Kołdarz <jakub213x@gmail.com>
2026-02-13 16:27:17 +0100

    Initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="bg-terminal-dark min-h-screen w-full p-12">
+    <h2 class="text-terminal-green font-terminal text-2xl">
+      Initial version of the terminal chat application. Backend is running on
+      ASP.NET Core, while frontend is built with Vue.js.
+    </h2>
+  </div>
+</template>
